ecad835690dea1d6d39f410ad4615776ff569123
Nedis Barreto <nedisbarreto@gmail.com>
2017-04-11 22:54:54 -0400

    Nedis: tables Owner, Employee, Position and Province

---
+++ dbo.tblPosition.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[tblPosition]
+(
+	[positionID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [name] NVARCHAR(50) NOT NULL
+)
---
+++ dbo.tblProvince.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[tblProvince]
+(
+	[provinceID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [name] NVARCHAR(50) NOT NULL
+)
