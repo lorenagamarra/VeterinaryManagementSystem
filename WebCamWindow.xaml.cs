4ec600143265b867d222570b8e84c1aa78ab4f9e
LorenaGamarra <lorenagamarra@gmail.com>
2017-04-24 11:27:12 -0400

    Lorena: WebCam access, files

---
+++ WebCamWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VeterinaryManagementSystem
+{
+    /// <summary>
+    /// Interaction logic for WebCamWindow.xaml
+    /// </summary>
+    public partial class WebCamWindow : Window
+    {
+        public WebCamWindow()
+        {
+            InitializeComponent();
+        }
+        WebCam webcam;
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            // TODO: Add event handler implementation here.
+            webcam = new WebCam();
+            webcam.InitializeWebCam(ref imgVideo);
+        }
+
+        private void bntStart_Click(object sender, RoutedEventArgs e)
+        {
+            webcam.Start();
+        }
+
+        private void bntStop_Click(object sender, RoutedEventArgs e)
+        {
+            webcam.Stop();
+        }
+
+        private void bntContinue_Click(object sender, RoutedEventArgs e)
+        {
+            webcam.Continue();
+        }
+
+        private void bntCapture_Click(object sender, RoutedEventArgs e)
+        {
+            imgCapture.Source = imgVideo.Source;
+        }
+
+        private void bntSaveImage_Click(object sender, RoutedEventArgs e)
+        {
+            Helper.SaveImageCapture((BitmapSource)imgCapture.Source);
+        }
+
+        private void bntResolution_Click(object sender, RoutedEventArgs e)
+        {
+            webcam.ResolutionSetting();
+        }
+
+        private void bntSetting_Click(object sender, RoutedEventArgs e)
+        {
+            webcam.AdvanceSetting();
+        }
+    }
+}
