253481166a60b4e74ccaea84ae25ead335e2ba56
Nedis Barreto <nedisbarreto@gmail.com>
2017-04-25 10:39:46 -0400

    Nedis: Animal Registry 50%, Employee Registry 5%

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -47,6 +47,7 @@ namespace VeterinaryManagementSystem
 
         private OwnerBusiness ownerBusiness;
         private AnimalBusiness animalBusiness;
+        private EmployeeBusiness employeeBusiness;
 
 
 
@@ -82,6 +83,7 @@ namespace VeterinaryManagementSystem
 
                 ownerBusiness = new OwnerBusiness();
                 animalBusiness = new AnimalBusiness();
+                employeeBusiness = new EmployeeBusiness();
 
             }
             catch (SqlException e)
@@ -320,6 +322,16 @@ namespace VeterinaryManagementSystem
 
 
 
+
+
+
+
+
+
+
+
+
+
         /******************************************************************************************
         * REGISTRY => ANIMAL
         ******************************************************************************************/
@@ -361,11 +373,8 @@ namespace VeterinaryManagementSystem
             }
             Animal animal = animalList[index];
 
-            //General Information for both owners
             tbRegistryAnimalDateRegistration.Text = animal.Datereg.ToString();
             tbRegistryAnimalID.Text = animal.Id.ToString();
-
-
             //imgRegistryAnimalPicture.Source = animal.Picture;     //TODO: Descobrir qual é a propriedade da imagem que guarda o que a imagem tem dentro..Content? Text?
             tbRegistryAnimalName.Text = animal.Name;
             dpRegistryAnimalBirthday.Text = animal.Dateofbirth.ToString();
@@ -426,15 +435,15 @@ namespace VeterinaryManagementSystem
                 Datereg = DateTime.Now.Date,                   // Add ok. mas update.. mudar a data de registro?????????????????
                 Name = tbRegistryAnimalName.Text,
                 Gender = gb_rb_AnimalGender.Content.ToString(), //radio button group,
-                //Dateofbirth = dpRegistryAnimalBirthday  ,   // date picker 
-                //Weight = tbRegistryAnimalWeight ,            //decimal
+                //Dateofbirth = dpRegistryAnimalBirthday  ,     // date picker 
+                //Weight = tbRegistryAnimalWeight ,             //decimal
                 Specie = cbRegistryAnimalSpecies.Text,
                 Identification = tbRegistryAnimalIdentification.Text,
                 Food = tbRegistryAnimalFood.Text,
                 Phobia = tbRegistryAnimalPhobias.Text,
-                //Flagset = ?????,                             //Conjunto de checkbox salvos em uma coluda separados por ,
+                //Flagset = ?????,                               //Conjunto de checkbox salvos em uma coluda separados por ,
                 Vethistoric = tbRegistryAnimalVetHistory.Text,
-                Status = gb_rb_AnimalStatus.Content.ToString()  //radio button group
+                Status = gb_rb_AnimalStatus.Content.ToString()   //radio button group
             };
 
             try
@@ -477,17 +486,21 @@ namespace VeterinaryManagementSystem
 
         private void AnimalForm_clearFields()
         {
-
-
-
+            tbRegistryAnimalDateRegistration.Text = DateTime.Now.ToString();   //Data no campo Data Registration????????????
+            tbRegistryAnimalID.Text = String.Empty;                             
+            imgRegistryAnimalPicture.Source = null;                            //Limpar imagem????????????????????????????????????????????????
+            tbRegistryAnimalName.Text = String.Empty;
+            //dpRegistryAnimalBirthday.Text = DatePicker.
+            //tbRegistryAnimalWeight.Text = ;                                   //Decimal
+            cbRegistryAnimalSpecies.Items.Clear();                              //comboBOX
+            cbRegistryAnimalBreeds.Items.Clear();                               //comboBOX
+            lvRegistryAnimalVaccines.Items.Clear();                             //LIST VIEW
+            tbRegistryAnimalIdentification.Text = String.Empty;
+            tbRegistryAnimalFood.Text = String.Empty;
+            tbRegistryAnimalPhobias.Text = String.Empty;
+            //FLAGSET.Items.Clear();                                            //CHECKBOXES FLAG SET
+            tbRegistryAnimalVetHistory.Text = String.Empty;
         }
-
-
-
-
-
-
-
 
         //TODO: Onde usar isso??? Verifica se o valor e nulo...
         private void dpRegistryAnimalBirthday_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
@@ -509,6 +522,211 @@ namespace VeterinaryManagementSystem
         }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /******************************************************************************************
+         * REGISTRY => EMPLOYEE
+         ******************************************************************************************/
+          
+        //LINQ - SEARCH ALL EMPLOYEE
+        private void tbRegistryEmployeeSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string filter = tbRegistryEmployeeSearch.Text.ToLower();
+            if (filter == "")
+            {
+                lvRegistryEmployeeSearchResult.ItemsSource = dbEmployee.GetAllEmployees();
+            }
+            else
+            {
+                List<Employee> list = dbEmployee.GetAllEmployees();
+                var filteredList = from e in list where e.Id.ToString().ToLower().Contains(filter) || (e.FirstName + " " + e.MiddleName + " " + e.LastName).ToString().Contains(filter) select e;  //primeiro "E" em vermelho
+
+                lvRegistryOwnerSearchResult.ItemsSource = filteredList;
+            }
+        }
+
+        //Registry -> Employee -> Search List Result
+        private void lvRegistryEmployeeSearchResult_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = lvRegistryEmployeeSearchResult.SelectedIndex;
+            if (index < 0)
+            {
+                tbRegistryEmployeeID.ToString();
+                return;
+            }
+            Employee employee = employeeList[index];
+
+
+            tbRegistryOwnerID.Text = owner.Id.ToString();
+            //imgRegistryOwner1Image.Source = owner.Picture_01;  //TODO: Descobrir qual é a propriedade da imagem que guarda o que a imagem tem dentro..Content? Text?
+            tbRegistryOwner1FName.Text = owner.FirstName_01;
+            tbRegistryOwner1MName.Text = owner.MiddleName_01;
+            tbRegistryOwner1LName.Text = owner.LastName_01;
+            tbRegistryOwner1NumberAddress.Text = owner.Number_01;
+            tbRegistryOwner1Address.Text = owner.Address_01;
+            tbRegistryOwner1Complement.Text = owner.Complement_01;
+            tbRegistryOwner1City.Text = owner.City_01;
+            cbRegistryOwner1Province.Text = owner.Province_01;
+            tbRegistryOwner1PostalCode.Text = owner.PostalCode_01;
+            tbRegistryOwner1Phone.Text = owner.PhoneNumber_01;
+            tbRegistryOwner1OtherNumber.Text = owner.OtherPhoneNumber_01;
+            tbRegistryOwner1Email.Text = owner.Email_01;
+
+
+            string verifyOwnerCkbStatus = owner.Status;   //Atualizando Status radiobutton de acordo com o Owner ????????????
+            if (verifyOwnerCkbStatus == "ACTIVE")
+            {
+                rbOwnerStatus_Active.IsChecked = true;
+                rbOwnerStatus_Inactive.IsChecked = false;
+            }
+            else
+            {
+                rbOwnerStatus_Active.IsChecked = false;
+                rbOwnerStatus_Inactive.IsChecked = true;
+            }
+        }
+
+        //Registry -> Owner -> Buttons Save/Add Record Event
+        private void btnRegistryOwnerSave_Click(object sender, RoutedEventArgs e)
+        {
+            SavingOwnerRegistryOnDB();
+        }
+
+
+        //Registry -> Owner -> Method Saving to DB
+        private void SavingOwnerRegistryOnDB()
+        {
+            var owner = new Owner
+            {
+                RegistrationDate = DateTime.Now.Date,                   // Add ok. mas update.. mudar a data de registro?????????????????
+
+                //Owner 1
+                //Picture_01 = imgRegistryOwner1Image.              ????????????????????????????????????????????
+                FirstName_01 = tbRegistryOwner1LName.Text,
+                MiddleName_01 = tbRegistryOwner1MName.Text,
+                LastName_01 = tbRegistryOwner1LName.Text,
+                Number_01 = tbRegistryOwner1NumberAddress.Text,
+                Address_01 = tbRegistryOwner1Address.Text,
+                Complement_01 = tbRegistryOwner1Complement.Text,
+                City_01 = tbRegistryOwner1City.Text,
+                Province_01 = cbRegistryOwner1Province.Text,          //combobox
+                PostalCode_01 = tbRegistryOwner1PostalCode.Text,
+                PhoneNumber_01 = tbRegistryOwner1Phone.Text,
+                OtherPhoneNumber_01 = tbRegistryOwner1OtherNumber.Text,
+                Email_01 = tbRegistryOwner1Email.Text,
+
+                //Owner 2
+                //Picture_02 = imgRegistryOwner2Image,                 ????????????????????????????????????????????
+                FirstName_02 = tbRegistryOwner2LName.Text,
+                MiddleName_02 = tbRegistryOwner2MName.Text,
+                LastName_02 = tbRegistryOwner2LName.Text,
+                Number_02 = tbRegistryOwner2NumberAddress.Text,
+                Address_02 = tbRegistryOwner2Address.Text,
+                Complement_02 = tbRegistryOwner2Complement.Text,
+                City_02 = tbRegistryOwner2City.Text,
+                Province_02 = cbRegistryOwner2Province.Text,         //combobox
+                PostalCode_02 = tbRegistryOwner2PostalCode.Text,
+                PhoneNumber_02 = tbRegistryOwner2Phone.Text,
+                OtherPhoneNumber_02 = tbRegistryOwner2OtherNumber.Text,
+                Email_02 = tbRegistryOwner2Email.Text,
+
+                Status = gb_rb_OwnerStatus.Content.ToString()       //radio button group
+            };
+
+            try
+            {
+                ownerBusiness.Save(owner);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        //Registry -> Owner -> Button Exit Event
+        private void btnRegistryOwnerExit_Click(object sender, RoutedEventArgs e)
+        {
+            if (unsavedChanges)
+            {
+                MessageBoxResult result = MessageBox.Show("Save unsaved changes?", "Unsaved changes", MessageBoxButton.YesNoCancel, MessageBoxImage.Warning);
+                switch (result)
+                {
+                    case MessageBoxResult.No:
+                        OwnerForm_clearFields();
+                        break;
+                    case MessageBoxResult.Cancel:
+                        break;
+                    case MessageBoxResult.Yes:
+                        SavingOwnerRegistryOnDB();
+                        break;
+                }
+            }
+        }
+
+
+        //Registry -> Owner -> Text changed on TextBox around the Registration window           //e os outros elementos diferentes de TB como combobom ou radiobutton??
+        private void tbOwnerTextChanged(object sender, TextChangedEventArgs e)
+        {
+            unsavedChanges = true;
+        }
+
+
+        //Registry -> Owner -> Method to clear the fields
+        private void OwnerForm_clearFields()
+        {
+            tbRegistryOwnerDateRegistration.Text = DateTime.Now.ToString();   //Data no campo Data Registration????????????
+            tbRegistryOwnerID.Text = String.Empty;
+
+            //Owner 1
+            imgRegistryOwner1Image = null;                    //Limpar imagem????????????????????????????????????????????????
+            tbRegistryOwner1FName.Text = String.Empty;
+            tbRegistryOwner1MName.Text = String.Empty;
+            tbRegistryOwner1LName.Text = String.Empty;
+            tbRegistryOwner1NumberAddress.Text = String.Empty;
+            tbRegistryOwner1Address.Text = String.Empty;
+            tbRegistryOwner1Complement.Text = String.Empty;
+            tbRegistryOwner1City.Text = String.Empty;
+            cbRegistryOwner1Province.Items.Clear();             //ComboBOX solucao que encontrei na internert para limpar item selecionado do elemento. certo??????
+            tbRegistryOwner1PostalCode.Text = String.Empty;
+            tbRegistryOwner1Phone.Text = String.Empty;
+            tbRegistryOwner1OtherNumber.Text = String.Empty;
+            tbRegistryOwner1Email.Text = String.Empty;
+
+
+            //Owner 2
+            imgRegistryOwner1Image = null;                    //Limpar imagem????????????????????????????????????????????????
+            tbRegistryOwner2FName.Text = String.Empty;
+            tbRegistryOwner2MName.Text = String.Empty;
+            tbRegistryOwner2LName.Text = String.Empty;
+            tbRegistryOwner2NumberAddress.Text = String.Empty;
+            tbRegistryOwner2Address.Text = String.Empty;
+            tbRegistryOwner2Complement.Text = String.Empty;
+            tbRegistryOwner2City.Text = String.Empty;
+            cbRegistryOwner2Province.Items.Clear();             //ComboBOX solucao que encontrei na internert para limpar item selecionado do elemento. certo??????
+            tbRegistryOwner2PostalCode.Text = String.Empty;
+            tbRegistryOwner2Phone.Text = String.Empty;
+            tbRegistryOwner2OtherNumber.Text = String.Empty;
+            tbRegistryOwner2Email.Text = String.Empty;
+
+            rbOwnerStatus_Active.IsChecked = true;                   //STATUS Deixar como default o ACTIVE
+        }
+
+        private void tbRegistrySearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
 
